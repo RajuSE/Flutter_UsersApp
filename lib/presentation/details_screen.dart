e722ffdd11ac27a625eccfde70cd8da6bac3c701
Raju <email22raju@gmail.com>
2024-07-10 22:08:37 +0530

    back press handling

--- lib/presentation/details_screen.dart
+++ lib/presentation/details_screen.dart
@@ -59,202 +59,215 @@ class _DetailsScreenState extends State<DetailsScreen> {
       }
     }
 
-    return Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-                icon: Icon(Icons.arrow_back,
-                    color: isDarkMode ? Colors.white : Colors.black),
-                onPressed: () {
-                  Provider.of<UserProvider>(context, listen: false)
-                      .isUpdateSuccess = false;
-                  Provider.of<UserProvider>(context, listen: false).isUpdating =
-                      false;
-                  Navigator.of(context).pop();
-                }),
-            title: const Text('')),
-        body:
-            // If the loading it true then it will show the circular progressbar
+    return WillPopScope(
+      onWillPop: (){
+        print("back invoked");
+        onExit(context);
+        return Future(() => true);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+              leading: IconButton(
+                  icon: Icon(Icons.arrow_back,
+                      color: isDarkMode ? Colors.white : Colors.black),
+                  onPressed: () {
+                    onExit(context);
+                  }),
+              title: const Text('')),
+          body:
+              // If the loading it true then it will show the circular progressbar
 
-            Stack(children: [
-          SingleChildScrollView(
-            child: Stack(
-              children: [
-                SizedBox(
-                  height: size.height,
-                  width: size.width,
-                ),
-                DelayedDisplay(
-                  fadeIn: showAnimation,
-                  delay: const Duration(milliseconds: 1500),
-                  child: Container(
-                    padding: const EdgeInsets.fromLTRB(22, 94, 22, 0),
-                    alignment: Alignment.topCenter,
-                    child: const Icon(Icons.person),
+              Stack(children: [
+            SingleChildScrollView(
+              child: Stack(
+                children: [
+                  SizedBox(
+                    height: size.height,
+                    width: size.width,
                   ),
-                ),
-                Positioned(
-                  top: 0,
-                  child: DelayedDisplay(
+                  DelayedDisplay(
                     fadeIn: showAnimation,
-                    delay: const Duration(milliseconds: 400),
-                    fadingDuration: const Duration(milliseconds: 400),
+                    delay: const Duration(milliseconds: 1500),
                     child: Container(
-                      width: size.width,
-                      height: size.height,
-                      padding: EdgeInsets.only(
-                          left: 16, right: 16, top: size.height / 45),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.background,
-                          borderRadius: const BorderRadius.vertical(
-                              top: Radius.circular(20))),
-                      child: Form(
-                        key: DetailsScreen._formKey,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Text("Name",
-                                style: Styles.tsDefaultSemiBold18),
-                            // const SizedBox(height: 24),
-                            const SizedBox(height: 8),
-                            AppTextField(
-                              controller: nameController,
-                              inputType: TextInputType.emailAddress,
-                              hintText: "Enter name",
-                              onChanged: (name) {},
-                            ),
-                            const SizedBox(height: 10),
-
-                            const Text("Email",
-                                style: Styles.tsDefaultSemiBold18),
-                            // const SizedBox(height: 24),
-                            const SizedBox(height: 8),
-                            AppTextField(
-                              controller: emailController,
-                              inputType: TextInputType.emailAddress,
-                              hintText: "Enter email ID",
-                              onChanged: (email) {},
-                            ),
-
-                            const SizedBox(height: 10),
-                            const Text("Phone",
-                                style: Styles.tsDefaultSemiBold18),
-                            // const SizedBox(height: 24),
-                            const SizedBox(height: 8),
-                            AppTextField(
-                              controller: phoneController,
-                              inputType: TextInputType.phone,
-                              hintText: "Enter phone",
-                              onChanged: (name) {},
-                            ),
-                            const SizedBox(height: 15),
-
-                            Row(children: [
-                              const Icon(Icons.person,
-                                  size: 24, color: Colors.blue),
-                              const SizedBox(
-                                width: 8,
+                      padding: const EdgeInsets.fromLTRB(22, 94, 22, 0),
+                      alignment: Alignment.topCenter,
+                      child: const Icon(Icons.person),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    child: DelayedDisplay(
+                      fadeIn: showAnimation,
+                      delay: const Duration(milliseconds: 400),
+                      fadingDuration: const Duration(milliseconds: 400),
+                      child: Container(
+                        width: size.width,
+                        height: size.height,
+                        padding: EdgeInsets.only(
+                            left: 16, right: 16, top: size.height / 45),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(20))),
+                        child: Form(
+                          key: DetailsScreen._formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Text("Name",
+                                  style: Styles.tsDefaultSemiBold18),
+                              // const SizedBox(height: 24),
+                              const SizedBox(height: 8),
+                              AppTextField(
+                                controller: nameController,
+                                inputType: TextInputType.emailAddress,
+                                hintText: "Enter name",
+                                onChanged: (name) {},
                               ),
-                              Flexible(
-                                child: Text("@${widget.user.username!}",
-                                    style: UiUtils.isDarkMode(context)
-                                        ? Styles.tsDefaultRegular18
-                                        : Styles.tsGreyTextBold16h18),
-                              ),
-                            ]),
+                              const SizedBox(height: 10),
 
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  const Icon(Icons.local_post_office,
-                                      size: 24, color: Colors.blue),
-                                  const SizedBox(
-                                    width: 8,
-                                  ),
-                                  Text("${widget.user.company!.name!}",
+                              const Text("Email",
+                                  style: Styles.tsDefaultSemiBold18),
+                              // const SizedBox(height: 24),
+                              const SizedBox(height: 8),
+                              AppTextField(
+                                controller: emailController,
+                                inputType: TextInputType.emailAddress,
+                                hintText: "Enter email ID",
+                                onChanged: (email) {},
+                              ),
+
+                              const SizedBox(height: 10),
+                              const Text("Phone",
+                                  style: Styles.tsDefaultSemiBold18),
+                              // const SizedBox(height: 24),
+                              const SizedBox(height: 8),
+                              AppTextField(
+                                controller: phoneController,
+                                inputType: TextInputType.phone,
+                                hintText: "Enter phone",
+                                onChanged: (name) {},
+                              ),
+                              const SizedBox(height: 15),
+
+                              Row(children: [
+                                const Icon(Icons.person,
+                                    size: 24, color: Colors.blue),
+                                const SizedBox(
+                                  width: 8,
+                                ),
+                                Flexible(
+                                  child: Text("@${widget.user.username!}",
                                       style: UiUtils.isDarkMode(context)
                                           ? Styles.tsDefaultRegular18
                                           : Styles.tsGreyTextBold16h18),
-                                ]),
+                                ),
+                              ]),
 
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Row(children: [
-                              const Icon(Icons.location_on,
-                                  size: 24, color: Colors.blue),
                               const SizedBox(
-                                width: 8,
+                                height: 12,
                               ),
-                              Flexible(
-                                child: Text(
-                                    '${widget.user.address!.street}, ${widget.user.address!.zipcode}, ${widget.user.address!.suite}, ${widget.user.address!.city}',
-                                    overflow: TextOverflow.clip,
-                                    style: UiUtils.isDarkMode(context)
-                                        ? Styles.tsDefaultRegular18
-                                        : Styles.tsGreyTextBold16h18),
-                              ),
-                            ]),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                          ],
-                        ),
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    const Icon(Icons.local_post_office,
+                                        size: 24, color: Colors.blue),
+                                    const SizedBox(
+                                      width: 8,
+                                    ),
+                                    Text("${widget.user.company!.name!}",
+                                        style: UiUtils.isDarkMode(context)
+                                            ? Styles.tsDefaultRegular18
+                                            : Styles.tsGreyTextBold16h18),
+                                  ]),
 
-                        // const SizedBox(height: 24),
-                        // SizedBox(height: size.height / 40),
-                        // const TrackOrderWidgets(),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                              Row(children: [
+                                const Icon(Icons.location_on,
+                                    size: 24, color: Colors.blue),
+                                const SizedBox(
+                                  width: 8,
+                                ),
+                                Flexible(
+                                  child: Text(
+                                      '${widget.user.address!.street}, ${widget.user.address!.zipcode}, ${widget.user.address!.suite}, ${widget.user.address!.city}',
+                                      overflow: TextOverflow.clip,
+                                      style: UiUtils.isDarkMode(context)
+                                          ? Styles.tsDefaultRegular18
+                                          : Styles.tsGreyTextBold16h18),
+                                ),
+                              ]),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                            ],
+                          ),
+
+                          // const SizedBox(height: 24),
+                          // SizedBox(height: size.height / 40),
+                          // const TrackOrderWidgets(),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
-              child: CustomFilledButton(
-                  isActive: true,
-                  color: AppColors.darkGreen,
-                  text: "Save",
-                  textStyle: Styles.tsWhiteTextSemiBold18,
-                  onTap: () {
-                    print("${phoneController.text.toString()}");
-                    print("${emailController.text.toString()}");
-                    print("${nameController.text.toString()}");
-                    UiUtils.dismissKeyboard(context);
-                    if (emailController.text.toString().isEmpty) {
-                      UiUtils.showSnackBar(context, "Please enter email id");
-                    } else if (phoneController.text.toString().trim().isEmpty) {
-                      UiUtils.showSnackBar(
-                          context, "Please enter phone number");
-                    } else if (phoneController.text
-                            .toString()
-                            .trim()
-                            .endsWith("-") ||
-                        phoneController.text
-                            .toString()
-                            .trim()
-                            .startsWith("-")) {
-                      UiUtils.showSnackBar(
-                          context, "Please enter valid phone number");
-                    } else if (nameController.text.toString().isEmpty) {
-                      UiUtils.showSnackBar(context, "Please enter name");
-                    } else {
-                      Provider.of<UserProvider>(context, listen: false)
-                          .updateUser(user.id, nameController.text,
-                              emailController.text, phoneController.text);
-                    }
-                    //tapped save btn
-                  }),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
+                child: CustomFilledButton(
+                    isActive: true,
+                    color: AppColors.darkGreen,
+                    text: "Save",
+                    textStyle: Styles.tsWhiteTextSemiBold18,
+                    onTap: () {
+                      print("${phoneController.text.toString()}");
+                      print("${emailController.text.toString()}");
+                      print("${nameController.text.toString()}");
+                      UiUtils.dismissKeyboard(context);
+                      if (emailController.text.toString().isEmpty) {
+                        UiUtils.showSnackBar(context, "Please enter email id");
+                      } else if (phoneController.text.toString().trim().isEmpty) {
+                        UiUtils.showSnackBar(
+                            context, "Please enter phone number");
+                      } else if (phoneController.text
+                              .toString()
+                              .trim()
+                              .endsWith("-") ||
+                          phoneController.text
+                              .toString()
+                              .trim()
+                              .startsWith("-")) {
+                        UiUtils.showSnackBar(
+                            context, "Please enter valid phone number");
+                      } else if (nameController.text.toString().isEmpty) {
+                        UiUtils.showSnackBar(context, "Please enter name");
+                      } else {
+                        Provider.of<UserProvider>(context, listen: false)
+                            .updateUser(user.id, nameController.text,
+                                emailController.text, phoneController.text);
+                      }
+                      //tapped save btn
+                    }),
+              ),
             ),
-          ),
-          if (value.isUpdating) const Center(child: CircularProgressIndicator())
-        ]));
+            if (value.isUpdating) const Center(child: CircularProgressIndicator())
+          ])),
+    );
+
   }
+
+  void onExit(BuildContext context) {
+    Provider.of<UserProvider>(context, listen: false).isUpdateSuccess = false;
+    Provider.of<UserProvider>(context, listen: false).isUpdating = false;
+    Navigator.of(context).pop();
+  }
+
+
+
 }
